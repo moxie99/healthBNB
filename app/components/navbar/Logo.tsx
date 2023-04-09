5e06a2dc6285f990eb56fcb1c2a01da1107a504d
Adeolu David <adeolusegun99@gmail.com>
2023-04-09 03:44:00 +0100

    feat: completed the navbar ui and set up modal for reusability

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,18 @@
+"use client";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+const Logo = () => {
+  const router = useRouter();
+  return (
+    <Image
+      src="/images/logo.png"
+      height="100"
+      width="100"
+      alt="Logo"
+      className="hidden md:block cursor-pointer"
+      priority={true}
+    />
+  );
+};
+
+export default Logo;
---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,24 @@
+"use-cliemt";
+import React from "react";
+import Container from "../Container";
+import Logo from "./Logo";
+import Search from "./Search";
+import ProfileMenu from "./ProfileMenu";
+
+const Navbar = () => {
+  return (
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div className="py-4 border-b-[1px]">
+        <Container>
+          <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
+            <Logo />
+            <Search />
+            <ProfileMenu />
+          </div>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
